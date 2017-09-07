e4e739b1129a3c62d9b379df42f29ffc6a1bd766
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-07 13:18:22 -0700

    ex 14 & 15

---
+++ Course/ex15.py
@@ -0,0 +1,15 @@
+#This prints the file given
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the name again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+print txt_again.read()
